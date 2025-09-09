1b448eacfef8a114b0a0bf2f59ddefa9442031a7
RayhanDerya <rayhanderya9@gmail.com>
2025-09-09 18:51:46 +0700

    Change Main.html

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
+        'title': 'The Kitman\'s Shop',
         'npm' : '2406403381',
         'name': 'Rayhan Derya Maheswara',
         'class': 'PBP B'
