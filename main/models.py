3ae432c278c7bc5b255840b05c010ac87cd0bbd1
RayhanDerya <rayhanderya9@gmail.com>
2025-09-09 13:46:30 +0700

    Tugas 2

---
+++ main/models.py
@@ -0,0 +1,21 @@
+import uuid
+from django.db import models
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('menswear', 'Menswear'),
+        ('womenswear', 'Womenswear'),
+        ('kidswear', 'Kidswear'),
+        ('exclusive', 'Exclusive'),
+    ]
+    
+    
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    thumbnail = models.URLField(blank=True, null=True)
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
