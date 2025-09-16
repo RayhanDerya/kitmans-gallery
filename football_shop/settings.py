7343c66f232a51ac25bfe10e8ecdf89e4550c0d6
RayhanDerya <rayhanderya9@gmail.com>
2025-09-16 14:40:10 +0700

    Tugas 3

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -32,7 +32,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","rayhan-derya-kitmansgallery.pbp.cs.ui.ac.id"]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://rayhan-derya-kitmansgallery.pbp.cs.ui.ac.id",
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -60,7 +62,7 @@ ROOT_URLCONF = 'football_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Tambahkan konten baris ini
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 
+
 class Product(models.Model):
     CATEGORY_CHOICES = [
         ('menswear', 'Menswear'),
@@ -12,11 +13,21 @@ class Product(models.Model):
     
     
     name = models.CharField(max_length=255)
-    price = models.IntegerField()
+    price = models.IntegerField(default=0)
     description = models.TextField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
     thumbnail = models.URLField(blank=True, null=True)
     is_featured = models.BooleanField(default=False)
+    product_views = models.PositiveIntegerField(default=0)
+
     
     def __str__(self):
         return self.name
+    
+    @property
+    def is_product_hot(self):
+        return self.product_views > 20
+        
+    def increment_views(self):
+        self.product_views += 1
+        self.save()
